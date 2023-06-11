9a00e6693bae915044f5658503547580bb1a4619
Sam Sobotka <sobotkas@csp.edu>
2023-06-11 18:21:09 -0500

    Release 3: Added odds of survivors defeating the zombies, list of zombies or survivors that remain (including survivor health), option to re-run the simulation, and saving results to file, also added comments

--- ZombieWar.java
+++ ZombieWar.java
@@ -6,178 +6,197 @@ import java.io.*;
 // Zombie Project - Zach Kasen, Gianna Bedford, Samuel Sobotka
 public class ZombieWar {
 
-	public static void main(String[] args) {
-		
-		int numSurvivors = (int) (Math.random() * 19 + 1);
-		int numZombies = (int) (Math.random() * 19 + 1);
-		ArrayList<Survivor> survivors = new ArrayList<>();
-		ArrayList<Zombie> zombies = new ArrayList<>();
-        double odds = (double) numSurvivors / (numSurvivors + numZombies);
-        double percent = 0;
+    public static void main(String[] args) {
 
-		
-		int numScientists = 1;
-		int numCivilians = 1;
-		int numSoldiers = 1;
+        int numSurvivors = (int) (Math.random() * 19 + 1); // Random number of survivors
+        int numZombies = (int) (Math.random() * 19 + 1); // Random number of survivors
 
-		for (int s = 0; s < numSurvivors; s++) {
+        ArrayList<Survivor> survivors = new ArrayList<>(); // Survivor array
+        ArrayList<Zombie> zombies = new ArrayList<>(); // Zombie array
 
-			int survivorType = (int) (Math.random() * 3);
+        double odds = (double) numSurvivors / (numSurvivors + numZombies); // Survivor percentage (survivors to total, less than 50% means survivors are outnumbered)
 
-			survivors.add(switch (survivorType) {
-				case 0 -> new Scientist("Scientist " + numScientists++);
-				case 1 -> new Civilian("Civilian " + numCivilians++);
-				case 2 -> new Soldier("Soldier " + numSoldiers++);
-				default ->
-						throw new IllegalStateException(String.format("Survivor type number %d does not correspond to a possible survivor type.", survivorType));
-			});
+        // counters for each survivor type
+        int numScientists = 0;
+        int numCivilians = 0;
+        int numSoldiers = 0;
 
-		}
+        // Generate survivor array
+        for (int s = 0; s < numSurvivors; s++) {
 
-		System.out.printf("There are %d survivors trying to make it to safety. There are %d scientists, %d civilians, and %d soldiers.\n",
-				survivors.size(), --numScientists, --numCivilians, --numSoldiers);
+            int survivorType = (int) (Math.random() * 3);
 
-		int numCommonInfected = 1;
-		int numTanks = 1;
+            survivors.add(switch (survivorType) {
+                case 0 -> new Scientist("Scientist " + ++numScientists); // Type 0 -> Scientist
+                case 1 -> new Civilian("Civilian " + ++numCivilians); // Type 1 -> Civilian
+                case 2 -> new Soldier("Soldier " + ++numSoldiers); // Type 2 -> Soldier
+                default -> // Any other type is invalid
+                        throw new IllegalStateException(String.format("Survivor type number %d does not correspond to a possible survivor type.", survivorType));
+            });
 
-		for (int z = 0; z < numZombies; z++) {
+        }
 
-			int zombieType = (int) (Math.random() * 4);
+        System.out.printf("There are %d survivors trying to make it to safety. There are %d scientists, %d civilians, and %d soldiers.\n",
+                survivors.size(), numScientists, numCivilians, numSoldiers);
 
-			zombies.add(switch (zombieType) {
-				case 0,1,2 -> new CommonInfected("CommonInfected " + numCommonInfected++);
-				case 3 -> new Tank("Tank " + numTanks++);
-				default ->
-						throw new IllegalStateException(String.format("Zombie type number %d does not correspond to a possible zombie type.", zombieType));
-			});
+        // counters for each zombie type
+        int numCommonInfected = 0;
+        int numTanks = 0;
 
-		}
+        // Generate zombie array
+        for (int z = 0; z < numZombies; z++) {
 
-		System.out.printf("But %d zombies are waiting for them. The survivors face %d common infected and %d tanks.\n",
-				zombies.size(), --numCommonInfected, --numTanks);
+            int zombieType = (int) (Math.random() * 4);
 
-		System.out.println();
+            zombies.add(switch (zombieType) {
+                case 0,1,2 -> new CommonInfected("CommonInfected " + ++numCommonInfected); // 75% chance for common infected
+                case 3 -> new Tank("Tank " + ++numTanks); // 25% chance for tank
+                default -> // Any other type is invalid
+                        throw new IllegalStateException(String.format("Zombie type number %d does not correspond to a possible zombie type.", zombieType));
+            });
+
+        }
+
+        System.out.printf("But %d zombies are waiting for them. The survivors face %d common infected and %d tanks.\n",
+                zombies.size(), numCommonInfected, numTanks);
+
+        System.out.println();
         System.out.printf("Odds of survivors winning: %.2f%%\n", odds * 100);
 
+        // Main loop -- runs until either all survivors or all zombies die
+        while (!survivors.isEmpty() && !zombies.isEmpty()) {
 
-		while (!survivors.isEmpty() && !zombies.isEmpty()) {
+            for (Survivor survivor : survivors) { // Each survivor
 
-			for (Survivor survivor : survivors) {
+                for (Zombie zombie : zombies) { // attacks every zombie
 
-				for (Zombie zombie : zombies) {
+                    survivor.attack(zombie);
 
-					survivor.attack(zombie);
+                    if (zombie.isDead()) {
+                        zombies.remove(zombie);
+                        printDeathMessage(survivor, zombie);
+                        break; // Do not continue attacking a dead zombie
+                    }
 
-					if (zombie.isDead()) {
-						zombies.remove(zombie);
-						printDeathMessage(survivor, zombie);
-						break;
-					}
+                }
 
-				}
-
-			}
-		
-
-			for (Zombie zombie : zombies) {
-
-				for (Survivor survivor : survivors) {
-
-					zombie.attack(survivor);
-
-					if (survivor.isDead()) {
-						survivors.remove(survivor);
-						printDeathMessage(zombie, survivor);
-						break;
-					}
-
-				}
-
-			}
-
-		}
-
-		System.out.println();
-
-		if (survivors.isEmpty()) {
-			System.out.println("None of the survivors made it. :(");
-			System.out.println("The zombie(s) remaining are:");
-            for (Zombie zombie : zombies) {
-                System.out.println(zombie);
             }
-            System.out.println("The zombies have taken over, all is lost");
-		} else {
-			System.out.printf("%d survivors have made it to safety!", survivors.size());
-			System.out.println("");
-			 System.out.println("The survivor(s) are:");
-	            for (Survivor survivor : survivors) {
-	                System.out.println(survivor);
-	            }
-	         System.out.println("Humanity lives to survive another day!");
-	         percent = survivors.size() / (double) numSurvivors;
-	            System.out.printf("Percent of survivors remaining: %.2f%%\n", percent * 100);
-	         
-	            
 
 
-		}
-		
-		   try (Scanner input = new Scanner(System.in)) {
-			System.out.println("Would you like to run the simulation again? (y/n)");
-	        String answer = input.nextLine();
-	        if (answer.equals("y")) {
-	            main(args);
-	        }
-	        else {
-	            System.out.println("Game over!");
-	            try {
-	                PrintWriter out = new PrintWriter("results.txt");
-	                out.println("Zombie War Results");
-	                out.println("The survivors that saved humanity:");
-	               
-	                
-	                for (Survivor survivor : survivors) {
-	                    out.println(survivor);
-	                }
-	                out.println("Zombies:");
-	                for (Zombie zombie : zombies) {
-	                    out.println(zombie);
-	                }
-	                out.println("Percentage of survivors remaining: "+ percent*100+ "%");
-	                System.out.println("Results saved...");
-	                out.close();
-	            } catch (FileNotFoundException e) {
-	                e.printStackTrace();
-	            }
-	         
+            for (Zombie zombie : zombies) { // Then, each zombie
 
-	        }
-		   }
-	
-	    }
-	
-	
-	 
+                for (Survivor survivor : survivors) { // attacks every survivor
 
-	private static void printDeathMessage(Survivor killer, Zombie victim) {
+                    zombie.attack(survivor);
 
-		switch (killer.getClass().getSimpleName()) {
-			case "Scientist" -> System.out.printf("%s was slapped by %s\n", victim.getName(), killer.getName());
-			case "Civilian" -> System.out.printf("%s was speared by %s\n", victim.getName(), killer.getName());
-			case "Soldier" -> System.out.printf("%s was shot by %s\n", victim.getName(), killer.getName());
-		}
+                    if (survivor.isDead()) {
+                        survivors.remove(survivor);
+                        printDeathMessage(zombie, survivor);
+                        break; // Do not continue attacking a dead survivor
+                    }
 
-	}
+                }
 
-	private static void printDeathMessage(Zombie killer, Survivor victim) {
+            }
 
-		switch (killer.getClass().getSimpleName()) {
-			case "CommonInfected" -> System.out.printf("%s was eaten by %s\n", victim.getName(), killer.getName());
-			case "Tank" -> System.out.printf("%s was crushed by %s\n", victim.getName(), killer.getName());
-		}
+        }
 
-	}
-	
+        System.out.println();
+        double percent = 0; // Percent of survivors that made it to safety
+
+        if (survivors.isEmpty()) { // Zombies ate everyone
+
+            System.out.println("None of the survivors made it. ☠");
+            System.out.println();
+            System.out.println("The zombie(s) remaining are:");
+
+            for (Zombie zombie : zombies)
+                System.out.println(zombie.getName());
+
+            System.out.println("The zombies have taken over; all is lost!");
+
+        } else {
+
+            System.out.printf("%d survivors have made it to safety!\n", survivors.size());
+            System.out.println();
+            System.out.println("The survivor(s) are:");
+
+            // Print name and health of each survivor (some might need medical attention :P)
+            for (Survivor survivor : survivors)
+                System.out.printf("%s Health: %d\n", survivor.getName(), survivor.getHealth());
+
+            System.out.println("Humanity lives to survive another day!");
+            percent = survivors.size() / (double) numSurvivors; // Percent calculation
+            System.out.printf("Percent of survivors remaining: %.2f%%\n", percent * 100);
+
+        }
+
+        try (Scanner input = new Scanner(System.in)) {
+
+            System.out.println("Would you like to run the simulation again? (y/n)");
+            String answer = input.nextLine();
+
+            if (answer.equalsIgnoreCase("y"))
+                main(args); // Re-run the program
+            else {
+
+                System.out.println("Game over!");
+
+                // Open file and append new results
+                try (PrintWriter out = new PrintWriter(new FileWriter("results.txt", true))) {
+
+                    out.println("\nZombie War Results");
+                    out.println("==================");
+
+                    if (survivors.isEmpty()) {
+
+                        out.println("The zombies have taken over!\n");
+                        out.println("Zombies remaining:");
+
+                        for (Zombie zombie : zombies)
+                            out.println(zombie.getName());
+
+                    } else {
+
+                        out.println("Humanity survives another day!");
+                        out.println("The survivors that saved humanity:");
+
+                        for (Survivor survivor : survivors)
+                            out.println(survivor.getName());
+
+                    }
+
+                    out.println("Percentage of survivors remaining: " + percent * 100 + "%");
+                    out.println("\n\n\n");
+                    System.out.println("Results saved...");
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }
+
+    }
+
+    private static void printDeathMessage(Survivor killer, Zombie victim) {
+
+        switch (killer.getClass().getSimpleName()) { // Print custom death message by simple class name
+            case "Scientist" -> System.out.printf("%s was slapped by %s\n", victim.getName(), killer.getName());
+            case "Civilian" -> System.out.printf("%s was speared by %s\n", victim.getName(), killer.getName());
+            case "Soldier" -> System.out.printf("%s was shot by %s\n", victim.getName(), killer.getName());
+        }
+
+    }
+
+    private static void printDeathMessage(Zombie killer, Survivor victim) {
+
+        switch (killer.getClass().getSimpleName()) { // Print custom death message by simple class name
+            case "CommonInfected" -> System.out.printf("%s was eaten by %s\n", victim.getName(), killer.getName());
+            case "Tank" -> System.out.printf("%s was crushed by %s\n", victim.getName(), killer.getName());
+        }
+
+    }
 
 }
-
